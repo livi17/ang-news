4928e9bfc53b3acca385ee36977f0634fd36fb7c
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-04-12 00:44:22 -0400

    Add Auth.js Controller

--- app/scripts/controllers/auth.js
+++ app/scripts/controllers/auth.js
@@ -1,1 +1,15 @@
-auth.js
+'use strict';
+
+app.controller('AuthCtrl', function ($scope, $location, Auth, user) {
+  if (user) {
+    $location.path('/');
+  }
+
+  $scope.register = function () {
+    Auth.register($scope.user).then(function() {
+      return Auth.login($scope.user).then(function() {
+        $location.path('/');
+      });
+    });
+  };
+});
