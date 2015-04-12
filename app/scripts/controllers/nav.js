b666dd52fb3b458fa3be50bddb5a3b4ef1ae911b
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-04-12 11:44:26 -0400

    Associate Users with Posts

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -6,6 +6,8 @@ app.controller('NavCtrl',   function ($scope, $location, Post, Auth) {
 	$scope.logout = Auth.logout;
 	
 	$scope.submitPost = function () {
+		$scope.post.creator = $scope.user.profile.username;
+		$scope.post.creatorUID = $scope.user.uid;
 		Post.create($scope.post).then(function (ref) {
 			$location.path('/posts/' + ref.name());
 			$scope.post = {url: 'http://', title: ''};
