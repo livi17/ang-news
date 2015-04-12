86ba797e6fb97694d2783bfadc126b53dcb9fff9
Michael LaRiviere <michael.lariviere@aceroinc.ca>
2015-04-12 01:48:30 -0400

    Added username creation

--- app/scripts/services/auth.js
+++ app/scripts/services/auth.js
@@ -1,12 +1,21 @@
 'use strict';
 
-app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope) {
+app.factory('Auth', function ($firebaseSimpleLogin, FIREBASE_URL, $rootScope, $firebase) {
 	var ref = new Firebase(FIREBASE_URL);
 	var auth = $firebaseSimpleLogin(ref);
 
 	var Auth = {
 		register: function (user) {
 			return auth.$createUser(user.email, user.password);
+		},
+		createProfile: function (user) {
+		  var profile = {
+		    username: user.username,
+		    md5_hash: user.md5_hash
+		  };
+
+		  var profileRef = $firebase(ref.child('profile'));
+		  return profileRef.$set(user.uid, profile);
 		},
 		login: function (user) {
 			return auth.$login('password', user);
